eeab52782cdf27a5851621eec16f984c492246e5
Renata Pedroso <renatanicollyy1@gmail.com>
2021-07-23 11:39:15 -0300

    domain class creation

---
+++ src/main/java/com/renata/bookstore/domain/Book.java
@@ -0,0 +1,90 @@
+package com.renata.bookstore.domain;
+
+public class Book {
+
+	private Integer id;
+	private String title;
+	private String autor_name;
+	private String text;
+
+	private Category category;
+
+	public Book() {
+		super();
+	}
+
+	public Book(Integer id, String title, String autor_name, String text, Category category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.autor_name = autor_name;
+		this.text = text;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAutor_name() {
+		return autor_name;
+	}
+
+	public void setAutor_name(String autor_name) {
+		this.autor_name = autor_name;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
