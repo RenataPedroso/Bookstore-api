8ff8b754128d67c3f95acb2a2641bd94861644bf
Renata Pedroso <renatanicollyy1@gmail.com>
2021-07-26 10:31:33 -0300

    completion of domain classes

--- src/main/java/com/renata/bookstore/domain/Book.java
+++ src/main/java/com/renata/bookstore/domain/Book.java
@@ -1,7 +1,18 @@
 package com.renata.bookstore.domain;
 
-public class Book {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String autor_name;
