7ce7489745dcc7056c42baed216e06b729562bfa
Renata Pedroso <renatanicollyy1@gmail.com>
2021-07-28 10:33:39 -0300

    connection to database h2

--- src/main/java/com/renata/bookstore/repositories/BookRepository.java
+++ src/main/java/com/renata/bookstore/repositories/BookRepository.java
@@ -3,9 +3,9 @@ package com.renata.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.renata.bookstore.domain.Category;
+import com.renata.bookstore.domain.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Category, Integer>{
+public interface BookRepository extends JpaRepository<Book, Integer>{
 
 }
